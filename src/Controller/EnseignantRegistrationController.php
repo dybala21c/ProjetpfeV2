a2d71a28f66875807d06429a82403a73b851acf1
conde <condemamadylamine@gmail.com>
2020-07-11 02:52:26 +0100

    mise à jour du login enseignant dans le template user

--- src/Controller/EnseignantRegistrationController.php
+++ src/Controller/EnseignantRegistrationController.php
@@ -45,35 +45,6 @@ class EnseignantRegistrationController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
-
-
-    /**
-     * @Route("/enseignant/connexion", name="app_login3")
-     */
-    public function loginEnseignant(AuthenticationUtils $authenticationUtils): Response
-    {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login_enseignant.html.twig', [
-            'last_username' => $lastUsername, 'error' => $error
-            ]);
-    }
-
-    /** 
-     * @Route("/logout", name="app_logout")
-     */
-    public function logout()
-    {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }
-    
     
      /**
      * @Route("/liste", name="list_enseignant", methods={"GET"})
